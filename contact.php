0a11b469e7d3dab147507659c6ac693b0dbd8f3f
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2022-03-08 19:02:51 +0300

    phpmailer

--- contact.php
+++ contact.php
@@ -1,16 +1,31 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
+require 'PHPMailer-5.2-stable/PHPMailerAutoload.php';
 
-$email_form = 'lizabayo@outlook.com';
-$email_subject = "New Form Submission";
-$email_body = "User Name: $name\n".
-               "User Email: $visitor_email.\n".
-               "User Message: $message.\n";
-$to = "lalabayo5@gmail.com";
-$headers = "From: $email_form \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
-mail($to,$email_subject,$email_body,$headers);
+$mail = new PHPMailer;
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp1.example.com;';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'lalabayo5@gmail.com';                 // SMTP username
+$mail->Password = 'secret';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->setFrom('lalabayo5@gmail.com', 'Admin');
+$mail->addAddress('lizabayo@outlook.com', 'Abayo');     // Add a recipient
+
+                               // Set email format to HTML
+
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
 header("Location: home.php"); 
-?>
