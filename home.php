f604a181ce22a99a4572b6f2d5a1fc233f9f0616
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2022-03-08 10:42:16 +0300

    update

--- home.php
+++ home.php
@@ -6,17 +6,18 @@ error_reporting(0);
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-  <title>Visitor Management System</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
-
-  <!-- Favicons -->
-  
-  
-
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Abayo Akinyi" />
+    <!--Descriptions-->
+    <meta name="description" content="I am a great developer for web development" />
+    <!--Keywords-->
+    <meta name="keywords" content="visitor, Management, System" />
+ 
+    <!--Main Style -->
+    <link rel="stylesheet" href="assets/css/home.css">
+    <title>Visitor Management System</title>
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
@@ -28,9 +29,6 @@ error_reporting(0);
   <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
   <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="assets/css/home.css" rel="stylesheet">
 
 </head>
 
